ac62f2e2783b13b3a86f79ef2275dcc192a87490
Sergiu <143271705+srsau@users.noreply.github.com>
2025-01-02 11:16:08 +0200

    init db

---
+++ scripts/setup_database.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS hotel;
+CREATE DATABASE hotel;
+USE hotel;
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role ENUM('customer', 'admin') NOT NULL DEFAULT 'customer'
+);
+
+INSERT INTO users (username, password, role) VALUES 
+('customer', 'customer', 'customer'), 
+('admin', 'admin', 'admin');
