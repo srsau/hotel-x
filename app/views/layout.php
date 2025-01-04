cf93f0f86bb20609d8117ed12a4d6e55d9d1ffd7
Sergiu <143271705+srsau@users.noreply.github.com>
2025-01-04 17:22:30 +0200

    remove About link from navigation

--- app/views/layout.php
+++ app/views/layout.php
@@ -23,9 +23,6 @@
                         <a class="nav-link" href="/">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/about">About</a>
-                    </li>
-                    <li class="nav-item">
                         <a class="nav-link" href="/contact">Contact</a>
                     </li>
                     <li class="nav-item">
