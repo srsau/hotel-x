a1a55fa82c39d56c9135b87c945beaa07be492d1
Sergiu <143271705+srsau@users.noreply.github.com>
2025-01-14 18:36:23 +0200

    update config

--- config/database.php
+++ config/database.php
@@ -4,6 +4,6 @@
 return [
     'host' => '127.0.0.1',
     'dbname' => 'hotel',
-    'user' => getenv('DB_USER'),
-    'password' => getenv('DB_PASS'),
+    'user' => getenv('DB_DAW_USER'),
+    'password' => getenv('DB_DAW_PASS'),
 ];
